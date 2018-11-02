913488434954fd8068fcb3849ac5751c633244db
Kelly Mears <code@kellymears.me>
2018-11-02 13:10:50 -0700

    update static path

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 
 app.use(function( req, res, next ) {
     switch( req.host ) {
-        case 'agit-pop.com': express.static( '/var/www/agit-pop.com/dist' )( req, res, next ); break;
+        case 'agit-pop.com': express.static( './agit-pop.com/dist' )( req, res, next ); break;
         default: res.redirect('https://tinypixel.io/404');
     }
 }).listen( 8080 );
