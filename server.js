dcc9205d284f9c0220c7a316d2b43f5b65ac2e4c
Kelly Mears <code@kellymears.me>
2018-11-02 13:25:20 -0700

    update for 443

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const http = require ('http');
+const https = require ('https')
+
+const app = express()
 
 app.use(function( req, res, next ) {
     switch( req.host ) {
         case 'agit-pop.com': express.static( './agit-pop.com/dist' )( req, res, next ); break;
         default: res.redirect('https://tinypixel.io/404');
     }
-}).listen( 8080 );
+});
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
+})
+
+httpServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
