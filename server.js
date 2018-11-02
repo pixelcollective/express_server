0af0eb95cb63339dcf0a9b6f61c3b1797b100458
Kelly Mears <code@kellymears.me>
2018-11-02 13:09:27 -0700

    updates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.().use(function( req, res, next ) {
+app.use(function( req, res, next ) {
     switch( req.host ) {
         case 'agit-pop.com': express.static( '/var/www/agit-pop.com/dist' )( req, res, next ); break;
         default: res.redirect('https://tinypixel.io/404');
